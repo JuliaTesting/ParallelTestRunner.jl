3aad0738cc944a51835d117fd61b1d0109cd0d1d
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-11-10 13:35:41 +0000

    Update src/ParallelTestRunner.jl
    Co-authored-by: Tim Besard <tim.besard@gmail.com>

--- src/ParallelTestRunner.jl
+++ src/ParallelTestRunner.jl
@@ -258,7 +258,8 @@ function runtest(f, name, init_code, color)
                 take!(pipe_initialized)
                 read(pipe, String)
             end
-            stats = redirect_stdio(; stdout=IOContext(pipe, :color=>$(color)), stderr=IOContext(pipe, :color=>$(color))) do
+            io = IOContext(pipe, :color=>$(color))
+            stats = redirect_stdio(; stdout=io, stderr=io) do
                 put!(pipe_initialized, nothing)
 
                 # @testset CustomTestRecord switches the all lower-level testset to our custom testset,
