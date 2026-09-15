d1a9f6a0162e09f5a83247ebcad8b4a347c3356d
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-09-15 20:53:58 -0300

    Put info in docstring

--- src/ParallelTestRunner.jl
+++ src/ParallelTestRunner.jl
@@ -532,21 +532,19 @@ end
 # `memory_per_worker` to `runtests`.
 const DEFAULT_MEMORY_PER_WORKER = 2 * Int64(2)^30
 
-# This is an internal function, not to be used by end users.  The
-# `_cpu_threads` and `_free_memory` keyword arguments are only for testing
-# purposes.
 """
-    default_njobs(; memory_per_worker = 2*2^30)
+    default_njobs(; memory_per_worker = 2 * 2^30,
+                    _cpu_threads = $(@static isdefined(Sys, :EFFECTIVE_CPU_THREADS) ? Symbol("Sys.EFFECTIVE_CPU_THREADS") : Symbol("Sys.CPU_THREADS")),
+                    _free_memory = ParallelTestRunner.available_memory())
 
-Determine default number of parallel jobs. Calculated as the number of CPU threads,
+*Internal* function used to determine the default number of parallel jobs. Calculated as the number of CPU threads,
 clamped such that each worker can be assumed to use `memory_per_worker` bytes of the
 available system memory.
 """
 function default_njobs(;
         memory_per_worker = DEFAULT_MEMORY_PER_WORKER,
-        ## private arguments for testing only
         # Just use Sys.EFFECTIVE_CPU_THREADS when min VERSION >= v"1.13"
-        _cpu_threads = (@static isdefined(Sys, :EFFECTIVE_CPU_THREADS) ? Sys.EFFECTIVE_CPU_THREADS : Sys.CPU_THREADS),
+        _cpu_threads = (@static isdefined(Sys, :EFFECTIVE_CPU_THREADS) ? Symbol("Sys.EFFECTIVE_CPU_THREADS") : Symbol("Sys.CPU_THREADS")),
         _free_memory = available_memory(),
     )
     memory_jobs = Int64(_free_memory) ÷ memory_per_worker
