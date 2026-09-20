a4b25e6746be6a4adb4ce67855996a00fee9a0dc
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-09-20 17:02:00 -0300

    Move some ancillary compat utils to separate file (#193)
    * Move some code closer to use
    
    * Move compat utilities to separate file

--- src/ParallelTestRunner.jl
+++ src/ParallelTestRunner.jl
@@ -16,32 +16,12 @@ import IOCapture
 using Test: DefaultTestSet
 using StyledStrings: Face, @styled_str, addface!
 
-function anynonpass(ts::Test.AbstractTestSet)
-    @static if VERSION >= v"1.13.0-DEV.1037"
-        return Test.anynonpass(ts)
-    else
-        Test.get_test_counts(ts)
-        return ts.anynonpass
-    end
+if VERSION >= v"1.13.0-DEV.1044"
+    using Base.ScopedValues
 end
 
-# Thin compatibility shim for using `Lockable` also in Julia v1.10
-if VERSION >= v"1.11.0-DEV.1568"
-    const Lockable = Base.Lockable
-else
-    # Adapted from <https://github.com/JuliaLang/julia/pull/52898>.
-    struct Lockable{T, L <: Base.AbstractLock}
-        value::T
-        lock::L
-    end
-
-    Lockable(value) = Lockable(value, ReentrantLock())
-    Base.getindex(l::Lockable) = (Base.assert_havelock(l.lock); l.value)
-
-    Base.lock(l::Lockable) = Base.lock(l.lock)
-    Base.trylock(l::Lockable) = Base.trylock(l.lock)
-    Base.unlock(l::Lockable) = Base.unlock(l.lock)
-end
+# anyonpass, Lockable, with_testset, available_memory
+include("compatutils.jl")
 
 const ID_COUNTER = Threads.Atomic{Int}(1)
 
@@ -58,6 +38,27 @@ function PTRWorker(; exename=Base.julia_cmd()[1], exeflags=String[], env=String[
     stdio_loop(wrkr, io)
     id = Threads.atomic_add!(ID_COUNTER, 1)
     return PTRWorker(wrkr, io, id)
+end
+
+
+# Adapted from `Malt._stdio_loop`, used by PTRWorker
+function stdio_loop(worker::Malt.Worker, io::Lockable)
+    Threads.@spawn while !eof(worker.stdout) && Malt.isrunning(worker)
+        try
+            bytes = readavailable(worker.stdout)
+            @lock io write(io[], bytes)
+        catch
+            break
+        end
+    end
+    Threads.@spawn while !eof(worker.stderr) && Malt.isrunning(worker)
+        try
+            bytes = readavailable(worker.stderr)
+            @lock io write(io[], bytes)
+        catch
+            break
+        end
+    end
 end
 
 worker_id(wrkr::PTRWorker) = wrkr.id
@@ -92,24 +93,6 @@ function get_max_worker_rss()
         1536
     end
     return mb * 2^20
-end
-
-function with_testset(f, testset)
-    @static if VERSION >= v"1.13.0-DEV.1044"
-        Test.@with_testset testset f()
-    else
-        Test.push_testset(testset)
-        try
-            f()
-        finally
-            Test.pop_testset()
-        end
-    end
-    return nothing
-end
-
-if VERSION >= v"1.13.0-DEV.1044"
-    using Base.ScopedValues
 end
 
 """
@@ -350,26 +333,6 @@ function truncate_line(line::AbstractString, max_width::Int)
     return line
 end
 
-# Adapted from `Malt._stdio_loop`
-function stdio_loop(worker::Malt.Worker, io::Lockable)
-    Threads.@spawn while !eof(worker.stdout) && Malt.isrunning(worker)
-        try
-            bytes = readavailable(worker.stdout)
-            @lock io write(io[], bytes)
-        catch
-            break
-        end
-    end
-    Threads.@spawn while !eof(worker.stderr) && Malt.isrunning(worker)
-        try
-            bytes = readavailable(worker.stderr)
-            @lock io write(io[], bytes)
-        catch
-            break
-        end
-    end
-end
-
 #
 # entry point
 #
@@ -484,58 +447,6 @@ function runtest(RecordType::Type{<:AbstractTestRecord}, f, name, init_code, sta
             Test.TESTSET_PRINT_ENABLE[] = old_print_setting
         end
     end
-end
-
-@static if Sys.isapple()
-
-mutable struct VmStatistics64
-	free_count::UInt32
-	active_count::UInt32
-	inactive_count::UInt32
-	wire_count::UInt32
-	zero_fill_count::UInt64
-	reactivations::UInt64
-	pageins::UInt64
-	pageouts::UInt64
-	faults::UInt64
-	cow_faults::UInt64
-	lookups::UInt64
-	hits::UInt64
-	purges::UInt64
-	purgeable_count::UInt32
-
-	speculative_count::UInt32
-
-	decompressions::UInt64
-	compressions::UInt64
-	swapins::UInt64
-	swapouts::UInt64
-	compressor_page_count::UInt32
-	throttled_count::UInt32
-	external_page_count::UInt32
-	internal_page_count::UInt32
-	total_uncompressed_pages_in_compressor::UInt64
-
-	VmStatistics64() = new(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-end
-
-
-function available_memory()
-	vms = Ref{VmStatistics64}(VmStatistics64())
-	mach_host_self = @ccall mach_host_self()::UInt32
-	count = UInt32(sizeof(VmStatistics64) ÷ sizeof(Int32))
-	ref_count = Ref(count)
-	@ccall host_statistics64(mach_host_self::UInt32, 4::Int64, pointer_from_objref(vms[])::Ptr{Int64}, ref_count::Ref{UInt32})::Int64
-
-	page_size = Int(@ccall sysconf(29::UInt32)::UInt32)
-
-	return (Int(vms[].free_count) + Int(vms[].inactive_count) + Int(vms[].purgeable_count) + Int(vms[].compressor_page_count)) * page_size
-end
-
-else
-
-available_memory() = Sys.free_memory()
-
 end
 
 # Assumed memory footprint of a single test worker, used to clamp the default
