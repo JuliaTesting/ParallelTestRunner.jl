bc42f34306cfe35ef3d088da5ead65c9df175e26
giordano <765740+giordano@users.noreply.github.com>
2026-09-23 00:42:14 +0000

    Delete history

---
+++ versions.js
@@ -0,0 +1,15 @@
+var DOC_VERSIONS = [
+  "stable",
+  "v2.9",
+  "v2.8",
+  "v2.7",
+  "v2.6",
+  "v2.5",
+  "v2.4",
+  "v2.3",
+  "v2.2",
+  "v2.1",
+  "dev",
+];
+var DOCUMENTER_NEWEST = "v2.9.1";
+var DOCUMENTER_STABLE = "stable";
